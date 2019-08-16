a25e909a5952dc56a5e8b6447d9dd5553b076665
Leo SIMON <lsimon@e1r8p15.42.fr>
2019-08-16 11:49:31 +0200

    server: from intra video

---
+++ inc/server.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsimon <lsimon@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/16 11:38:53 by lsimon            #+#    #+#             */
+/*   Updated: 2019/08/16 11:43:59 by lsimon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERVER_H
+# define SERVER_H
+
+# include "../libft/libft.h"
+# include <sys/socket.h>
+# include <netdb.h>
+# include <unistd.h>
+# include <netinet/in.h>
+# include <arpa/inet.h>
+
+# define BUF_SIZE 1024
+
+#endif
--- srcs/server/server.c
+++ srcs/server/server.c
@@ -1,4 +1,55 @@
-int main()
+#include "../../inc/server.h"
+
+int create_server(int port, int backlog)
 {
-	return 0;
+	int					sock;
+	struct protoent		*proto;
+	struct sockaddr_in	sin;
+
+	proto = getprotobyname("tcp");
+	if (proto == 0)
+		return (-1);
+	sock = socket(PF_INET, SOCK_STREAM, proto->p_proto);
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(port);
+	sin.sin_addr.s_addr = htonl(INADDR_ANY);
+	if (bind(sock, (const struct sockaddr *)&sin, sizeof(sin)) == -1)
+	{
+		ft_putstr("Bind errir\n");
+		return(-1);
+	}
+	listen(sock, backlog);
+	return (sock);
+}
+
+int usage()
+{
+	ft_putstr("Usage: ./server [port]\n");
+	return (1);
+}
+
+int main(int ac, char **av)
+{
+	int 				sock;
+	int 				port;
+	int 				cs;
+	int					r;
+	unsigned int 		cslen;
+	struct sockaddr_in	csin;
+	char 				buf[BUF_SIZE];
+
+
+
+	if (ac != 2)
+		return usage();
+
+	port = ft_atoi(av[1]);
+	sock = create_server(port, 42);
+	if (sock == -1)
+		return (1);
+	cs = accept(sock, (struct sockaddr *)&csin, &cslen);
+	r = read(cs, buf, BUF_SIZE - 1);
+	close(cs);
+	close(sock);
+	return (0);
 }
